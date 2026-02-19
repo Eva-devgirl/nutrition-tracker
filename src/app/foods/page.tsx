ad29e3d0a535cbc3b0e8f6f39b4adafd0bdcb031
Eva-devgirl <evatsigkana@yahoo.gr>
2026-02-19 12:58:42 +0100

    Initial commit - Next.js nutrition tracker

---
+++ src/app/foods/page.tsx
@@ -0,0 +1,9 @@
+export default function FoodsPAge() {
+  return (
+     <main style={{ padding: "40px", fontFamily: "sans-serif" }}>
+	<h1>Foods</h1>
+        <p>This is where your food items will appear.</p>
+     </main>
+   );
+ }
+
